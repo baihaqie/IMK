c337a81bb6f67a29c03aded77334433fd49352a3
baihaqie <haqiphoenix@yahoo.com>
2012-12-05 19:20:59 +0700

    cleaning gh-pages

--- js/reveal.js
+++
@@ -1,7 +0,0 @@
-function showStuff(id) {
-      document.getElementById(id).style.display = 'block';
-   }
-   function hideStuff(id) {
-      document.getElementById(id).style.display = 'none';
-   }
-}
